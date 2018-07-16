a5ac5303299d1428d981be652a82a03e3719734b
Keftcha <alban.dewilde@gmail.com>
2018-07-16 17:06:50 +0200

    Add comments and respect of the PEP 8

--- server.py
+++ server.py
@@ -30,32 +30,36 @@ host = arguments.host if arguments.host else "localhost"
 port = arguments.port if arguments.port else 21617
 max_clients = arguments.clients if arguments.clients else 10
 
+# Define server parameters
 server = socket.socket()
 server.bind((host, port))
 server.listen(max_clients)
 
-print("Listening on port {0} with a maximum of {1} clients\n".format(port, max_clients))
+print("The server is listening on port {0}\n".format(port))
 
 running = True
-clients = []
+clients = []    # List of all clients
 
 while running:
     # Accept connection of socket
     connecting_requests, _, _ = select.select([server], [], [], 0)
     for request in connecting_requests:
-        client, _ = request.accept()
+        client, info_client = request.accept()
         clients.append(client)
 
     if clients:
         rlist, _, _ = select.select(clients, [], [], 0)
         for client_talking in rlist:
             message = client_talking.recv(4096)
-            text = message.decode()[:-1] # Remove the "\n" at the
-            for client in clients:
-                client.send(message)
+            text = message.decode()[:-1]    # Remove the "\n" at the end
+
+            # Send message to all other connected clients
+            [client.send(message) for client in clients]
+
             print(time.strftime("%Y/%m/%d %H:%M:%S"))
             print(str(len(clients)) + " connected sockets")
             print("message send: " + text + "\n")
+
             if text == "leave()":
                 client_talking.close()
                 del clients[clients.index(client_talking)]
